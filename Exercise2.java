558742130baee4af5af254ea854117f35d666dcd
Camolas <up201202445@fe.up.pt>
2018-03-30 21:32:44 +0100

    Java exercises, URL:https://www.w3resource.com/java-exercises/basic/index.php#editorr

---
+++ Exercise2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+	public static void main(String[] args)
+	{
+		System.out.println();
+		Scanner sc = new Scanner (System.in);
+	    Scanner sc2 = new Scanner (System.in);
+	    int b; 
+	      int a = sc.nextInt();
+	      System.out.print("Input second integer: ");
+	      b = sc2.nextInt();
+	      int op1 = 74;
+	      int op2 = 36;
+	      System.out.println(74 + 36);
+	      System.out.println(op1 + op2);
+	      System.out.println("Sum: "+ (a+b));
+	}
+}
+
